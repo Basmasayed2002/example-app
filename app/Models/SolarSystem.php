5df37c0eb44746dcadcf97409d1cc37bb6abf4c8
Basmasayed2002 <2102395@talnet.nl>
2024-10-11 11:18:57 +0200

    .

--- app/Models/SolarSystem.php
+++ app/Models/SolarSystem.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\QueryException;
 
-class SolarSystem extends Model
+Class SolarSystem extends Model
 {
     use HasFactory;
 
